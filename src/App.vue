c9567d2fe67d7a188f71cbb6a8a529a568301442
ibrahimcankarta <ibrahimcankarta@gmail.com>
2021-07-25 18:32:49 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <MainComponent/>
+  </div>
+</template>
+
+<script>
+import MainComponent from './components/mainComponent.vue'
+export default {
+  name: 'App',
+  components: {
+    MainComponent
+  }
+  /*async created() {
+    var { data }=await axios.get("https://recruitment-mock-data.gjg-ads.io/data") ;
+    console.log(data)
+  }*/
+}
+</script>
+
+<style>
+
+</style>
